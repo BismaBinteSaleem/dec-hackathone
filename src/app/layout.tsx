db365dae13791b02b0a9844bf595fa29bf229cb1
BismaBinteSaleem <bintesyed31312@gmail.com>
2024-12-10 00:12:18 +0500

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import NavBar from "@/components/navBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,8 +30,11 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      > 
+      <NavBar/>
+      <Header/>
         {children}
+      <Footer/>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,72 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <div>
+        <div className="HeroImage">
+          <div className="herotext1">
+            <h1 className="text-[15px] text-center"><strong>Hello Nike APP</strong></h1>
+            <h2 className="text-[11px] text-center">Download the app to access everything Nike. Get your Great</h2>
+          </div>
+          <Image src="/images/heroShoes.jpg" alt="logo"layout="responsive" width={1344} height={700} />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div>
+          <h1 className="text-[15px] text-center pt-14 md:pt-58px">First Look</h1>
+          <h3 className="text-[56px] text-center pt-7 md:pt-28px"><strong>Nike Air Max Pulse</strong></h3>
+          <h1 className="text-[15px] text-center pt-28 md:pt-112px w-11/12 md:w-[511px] h-auto">Extreme comfort. Hyper durable. Max volume. Introducing the Air Max Pulse —designed to push you past your limits and help you go to the max.</h1>
+        </div>
+
+        <div className="flex justify-center items-center space-x-4 py-6">
+          <button className="w-[110.58px] h-[39px] bg-[#111111] text-white rounded-[30px] border border-solid border-black p-2">Notify Me</button>
+          <button className="w-[138.16px] h-[39px] bg-[#111111] text-white rounded-[30px] p-2">Shop Air Max</button>
+        </div>
+
+        <Image src="/images/BestofAirMax.png" alt="logo" layout="responsive"width={1344} height={700} />
+
+        <div>
+          <h1 className="text-[15px] font-bold pt-24 md:pt-84px">Featured</h1>
+          <Image src="/images/features.png" alt="logo" layout="responsive"width={1344} height={700} />
+        </div>
+
+        <div>
+          <h1 className="text-[52px] w-11/12 text-center pt-24 md:pt-96px"><strong>STEP INTO WHAT FEELS GOOD</strong></h1>
+          <h3 className="text-[15px] text-center pt-6 md:pt-84px">Cause everyone should know the feeling of running in that perfect pair.</h3>
+          <div className="flex justify-center items-center py-6">
+            <button className="w-[152.42px] h-[39px] bg-[#111111] text-white rounded-[30px] p-[7.5px_23.92px_7.5px_22.5px] flex items-center text-[15px]">
+              Find Your Shoe
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <h1 className="text-[15px] w-[90.9px] h-[27px] md:w-[512px] md:h-[60px] pt-12 md:pt-96px"><strong>Gear UP</strong></h1>
+        <div className="flex space-x-4">
+          <Image src="/images/gearup1.png" alt="logo" layout="responsive"width={666} height={447} />
+          <Image src="/images/gearup2.png" alt="logo" layout="responsive"width={666} height={447} />
+        </div>
+      </div>
+
+      <div>
+        <h1 className="text-[22px] w-[90.9px] h-[27px] md:w-[512px] md:h-[60px] pt-12 md:pt-52px"><strong>Don&apos;t Miss</strong></h1>
+        <div className="pt-[52px]">
+          <Image src="/images/Dontmiss.jpg" alt="logo"layout="responsive" width={1344} height={700} />
+        </div>
+      </div>
+
+      <div>
+        <h1 className="text-[52px] w-11/12 md:w-[512px] h-[60px] pt-12 md:pt-48px text-center"><strong>FLIGHT ESSENTIALS</strong></h1>
+        <h1 className="text-[15px] w-11/12 md:w-[512px] h-[24px] pt-6 md:pt-24px text-center">Your built-to-last, all-week wears—but with style only Jordan Brand can deliver.</h1>
+        <button className="pt-6 md:pt-30px w-[80.38px] h-[39px] bg-[#111111] text-white rounded-[30px] p-[7.5px_23.92px_7.5px_22.5px] flex items-center">Shop</button>
+      </div>
+
+      <div>
+        <h1 className="text-[52px] w-11/12 md:w-[512px] h-[27px] pt-12 md:pt-96px pb-20px" >The Essential</h1>
+        </div>
+        <div>
+        <Image src="/images/Thessential.png" alt="logo"layout="responsive" width={1356} height={540} />
+      </div>
     </div>
   );
 }
